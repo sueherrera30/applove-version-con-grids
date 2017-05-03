69e03ebd132490c8112cd42a2ab4fdf3ee0b95ed
sue martinez <sueherrera30@gmail.com>
2017-05-03 13:12:52 +0000

    ya el chido

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
-/*
- * Archivo principal de funcionalidad de JS
- */
+var modal = document.getElementById('fondoModal');
+var img = document.getElementsByClassName('img');
+var imgProducto = document.getElementById("img01");
+var borrar = document.getElementsByClassName("cerrar")[0];
+borrar.addEventListener("click", cerrar);
+
+for(i=0; i< img.length; i++){
+  img[i].addEventListener("click", popUp);
+}
+function popUp(){
+    modal.style.display = "block";
+    imgProducto.src = this.src;
+}
+function cerrar() {
+    modal.style.display = "none";
+}
